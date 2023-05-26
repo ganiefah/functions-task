753fa8c95a9e992ffc8068fbf708932ead3e00c0
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-26 11:39:05 +0200

    y

---
+++ script/code.js
@@ -0,0 +1,14 @@
+function greet(){
+    console.log('Hello');
+}
+greet()
+function multiply(x, y){
+    console.log(x*y);
+}
+multiply(2, 5);
+// function tableOfFive(x)
+var x = tableOfFive(5);
+function tableOfFive(x){
+    return x * 5;
+}
+console.log(x * 5)
